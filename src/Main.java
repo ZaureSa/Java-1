ee3ca5be7aa4fc2460b2ace42fd58dfec888eb98
Zaure <Zsaigutina@gmail.com>
2023-03-09 16:32:06 +0300

    src changed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        int TicketPrice = 54_899;
+        int TicketPrice = 63_456;
         int mile = 20;
 
-        int bonus = TicketPrice / 20;
+        int bonus = TicketPrice / mile;
 
         System.out.println("Итоговый бонус:" + bonus);
 
